cf268e57ef2a1f2315650529e09d80921042ba4a
QuySang <vanquysang1610@gmail.com>
2021-11-12 15:58:05 +0700

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,13 @@ const openBar = document.querySelector('.nav-item__bars')
 const closeBar = document.querySelector('.nav-item__bar-close')
 const overLay = document.querySelector('.mask')
 
-
-numberPerson.addEventListener('click', function() {
-    selectnumber.classList.add('select-number--opened')
-    numberPerson.classList.add('search__number--active')
+numberPerson.addEventListener('click', function(e) {
+        selectnumber.classList.add('select-number--opened')
+        numberPerson.classList.add('search__number--active')  
 })
-menuLanguage.addEventListener('click',function(){
-    openLanguage.classList.add('menu__language--opened')
+
+menuLanguage.addEventListener('click',function(e){
+        openLanguage.classList.add('menu__language--opened')
 })
 barBtn.addEventListener('click',function() {
     openBar.classList.add('nav-item__bars--open')
@@ -28,6 +28,9 @@ closeBar.addEventListener('click',function() {
 openBar.addEventListener('click', function(e) {
     const menuLanguage123 = e.target.closest('.js-menu-language')
     const openLanguage2 = document.querySelector('.menu__language2')
+    const openLanguage1 = document.querySelector('.menu__language--opened2')
+   
+
     if(menuLanguage123) {
         
         openLanguage2.classList.add('menu__language--opened2')
@@ -35,6 +38,14 @@ openBar.addEventListener('click', function(e) {
     else if(e.target != menuLanguage123) {
         openLanguage2.classList.remove('menu__language--opened2')
     }
+    if(openLanguage1) {
+        openLanguage2.classList.remove('menu__language--opened2')
+    }
+    
+})
+overLay.addEventListener('click',function() {
+    openBar.classList.remove('nav-item__bars--open')
+    overLay.style.display = "none"
 })
 window.addEventListener('mouseup', function(event) {
     
@@ -45,9 +56,5 @@ window.addEventListener('mouseup', function(event) {
         selectnumber.classList.remove('select-number--opened')
         numberPerson.classList.remove('search__number--active')
     }
-    if(event.target != openBar ){
-        openBar.classList.remove('nav-item__bars--open')
-        overLay.style.display = "none"
-    }
-
+    
  });
